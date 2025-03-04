45d1e3f4c5366b3c0fd0651d8e98c89633605d9b
Vincent van de Schaft <vincentvdschaft@gmail.com>
2025-03-04 08:50:43 +0100

    Update

--- plotlib/animation.py
+++ plotlib/animation.py
@@ -1,3 +1,12 @@
+import numpy as np
+
+
+def linear_clipped(t, factor=0.9):
+    t = (t - 0.5) / factor + 0.5
+
+    return np.clip(t, 0, 1)
+
+
 def smooth(t: float) -> float:
     """Smooth transition function for animations, copied from manim.
 
@@ -6,3 +15,15 @@ def smooth(t: float) -> float:
     """
     s = 1 - t
     return (t**3) * (10 * s * s + 5 * s * t + t * t)
+
+
+def map_range(t, start, end, init_start=0, init_end=1):
+    return start + (end - start) * (t - init_start) / (init_end - init_start)
+
+
+def smooth_range(t, start, end):
+    return map_range(smooth(t), start, end)
+
+
+def smooth_range(t, start, end):
+    return map_range(smooth(t), start, end)
